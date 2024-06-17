9c04dda963c40f63fef7dc43fd4491407492c1de
adnan Alkharfan <adnan@adnans-Air.home>
2024-06-17 22:08:37 +0300

    check if the code running in 2024

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -8,13 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate,MyPro {
+    
+    func didget(_ rate: Double) {
+        DispatchQueue.main.async {
+            self.price.text=String(format: "%.4f",rate)
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+      
+      
+        return c.currencyArray[row]
+    }
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        currency.text=c.currencyArray[row]
+        c.fetch(currency.text!)
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return c.currencyArray.count
+    }
+    var c=CoinManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        picker.dataSource=self
+        picker.delegate=self
+        c.delegte=self
         // Do any additional setup after loading the view.
     }
-
-
+    @IBOutlet weak var price: UILabel!
+    
+    @IBOutlet weak var currency: UILabel!
+    @IBOutlet weak var picker: UIPickerView!
+    
 }
 
--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -8,12 +8,42 @@
 
 import Foundation
 
+protocol MyPro {
+    func didget(_ rate:Double)
+}
+
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
-    
+    let apiKey = "1B65A800-0839-432B-9020-9AD3D160D8DF"
+    var delegte:MyPro?
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
+    func fetch(_ s:String) {
+        let url="\(baseURL)/\(s)?apikey=\(apiKey)"
+        
+        let connect=URL(string: url)!
+        
+        let session=URLSession(configuration: .default)
+        if connect != nil {
+        let task = session.dataTask(with: connect){
+            
+            if $2 != nil{return}
+            if $0 != nil {
+                
+                let jason = JSONDecoder()
+                do{
+           let result = try jason.decode(coin.self, from: $0!)
+                    delegte?.didget(result.rate)
+                }catch{}
+                
+            }
+        }
+        
+        
+            task.resume()}
+        
+    }
+    
     
 }
